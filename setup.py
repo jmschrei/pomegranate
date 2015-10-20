b4f15d11d329d8e2b140f639c88696ef46bbb6d6
Jacob Schreiber <jmschreiber91@gmail.com>
2015-10-19 22:42:01 -0700

    FIX joblib req

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ else:
 
 setup(
     name='pomegranate',
-    version='0.3.0',
+    version='0.3.1',
     author='Jacob Schreiber',
     author_email='jmschreiber91@gmail.com',
     packages=['pomegranate'],
@@ -48,7 +48,7 @@ setup(
     install_requires=[
         "cython >= 0.22.1",
         "numpy >= 1.8.0",
-        "joblib >= 0.90b4",
+        "joblib >= 0.9.0b4",
         "networkx >= 1.8.1"
     ],
 )
