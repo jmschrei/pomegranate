a76e78aeeebd26e2ac668658402a463d94c66322
Jacob Schreiber <jmschreiber91@gmail.com>
2016-03-10 20:03:55 -0800

    ENH 0.3.8

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ else:
 
 setup(
     name='pomegranate',
-    version='0.3.7',
+    version='0.3.8',
     author='Jacob Schreiber',
     author_email='jmschreiber91@gmail.com',
     packages=['pomegranate'],
