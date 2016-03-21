8a3631525a54f71a6f377ac6c1a4c006f33bfc4b
Jacob Schreiber <jmschreiber91@gmail.com>
2016-03-21 16:43:04 -0700

    ENH Bayes Net doc
    ENH bayes net doc

--- pomegranate/BayesianNetwork.pyx
+++ pomegranate/BayesianNetwork.pyx
@@ -12,8 +12,53 @@ from .FactorGraph import FactorGraph
 from .utils cimport _log
 
 cdef class BayesianNetwork( Model ):
-	"""
-	Represents a Bayesian Network
+	"""A Bayesian Network Model.
+
+	A Bayesian network is a directed graph where nodes represent variables, edges
+	represent conditional dependencies of the children on their parents, and the
+	lack of an edge represents a conditional independence. 
+
+	Example
+	-------
+	>>> from pomegranate import *
+	>>> d1 = DiscreteDistribution({'A': 0.2, 'B': 0.8})
+	>>> d2 = ConditionalProbabilityTable([['A', 'A', 0.1],
+												 ['A', 'B', 0.9],
+												 ['B', 'A', 0.4],
+												 ['B', 'B', 0.6]], [d1])
+	>>> s1 = Node( d1, name="s1" )
+	>>> s2 = Node( d2, name="s2" )
+	>>> model = BayesianNetwork()
+	>>> model.add_nodes([s1, s2])
+	>>> model.add_edge(s1, s2)
+	>>> model.bake()
+	>>> print model.log_probability(['A', 'B'])
+	-1.71479842809
+	>>> print model.predict_proba({'s2' : 'A'})
+	array([ {
+	    "frozen" :false,
+	    "class" :"Distribution",
+	    "parameters" :[
+	        {
+	            "A" :0.05882352941176471,
+	            "B" :0.9411764705882353
+	        }
+	    ],
+	    "name" :"DiscreteDistribution"
+	},
+	       {
+	    "frozen" :false,
+	    "class" :"Distribution",
+	    "parameters" :[
+	        {
+	            "A" :1.0,
+	            "B" :0.0
+	        }
+	    ],
+	    "name" :"DiscreteDistribution"
+	}], dtype=object)
+	>>> print model.impute([[None, 'A']])
+	[['B', 'A']]
 	"""
 
 	def bake( self, verbose=False ): 
@@ -86,9 +131,24 @@ cdef class BayesianNetwork( Model ):
 		self.graph.bake()
 
 	def log_probability( self, sample ):
-		"""
-		Return the log probability of the sample under the Bayesian network.
-		Currently only supports samples which are fully observed.
+		"""Return the log probability of a sample under the Bayesian network model.
+		
+		The log probability is just the sum of the log probabilities under each of
+		the components. The log probability of a sample under the graph A -> B is 
+		just P(A)*P(B|A).
+
+		Parameters
+		----------
+		sample : array-like, shape (n_nodes)
+			The sample is a vector of points where each dimension represents the
+			same variable as added to the graph originally. It doesn't matter what
+			the connections between these variables are, just that they are all
+			ordered the same.
+
+		Returns
+		-------
+		logp : double
+			The log probability of that sample.
 		"""
 
 		indices = { state.distribution: i for i, state in enumerate( self.states ) }
@@ -107,23 +167,91 @@ cdef class BayesianNetwork( Model ):
 		return logp
 
 	def marginal( self ):
-		"""
-		Return the marginal of the graph. This is equivilant to a pass of
-		belief propogation given that no data is given; or a single forward
-		pass of the sum-product algorithm.
+		"""Return the marginal probabilities of each variable in the graph.
+
+		This is equivalent to a pass of belief propogation on a graph where
+		no data has been given. This will calculate the probability of each
+		variable being in each possible emission when nothing is known.
+
+		Parameters
+		----------
+		None
+
+		Returns
+		-------
+		marginals : array-like, shape (n_nodes)
+			An array of univariate distribution objects showing the marginal
+			probabilities of that variable.
 		"""
 
 		return self.graph.marginal()
 
 	def predict_proba( self, data={}, max_iterations=100, check_input=True ):
-		"""sklearn wrapper for loopy belief propogation."""
+		"""Returns the probabilities of each variable in the graph given evidence.
+
+		This calculates the marginal probability distributions for each state given
+		the evidence provided through loopy belief propogation. Loopy belief
+		propogation is an approximate algorithm which is exact for certain graph
+		structures.
+
+		This is a sklearn wrapper for the forward_backward method.
+
+		Parameters
+		----------
+		data : dict or array-like, shape <= n_nodes, optional
+			The evidence supplied to the graph. This can either be a dictionary
+			with keys being state names and values being the observed values
+			(either the emissions or a distribution over the emissions) or an
+			array with the values being ordered according to the nodes incorporation
+			in the graph (the order fed into .add_states/add_nodes) and None for
+			variables which are unknown. If nothing is fed in then calculate the
+			marginal of the graph.
+		max_iterations : int, optional
+			The number of iterations with which to do loopy belief propogation.
+			Usually requires only 1.
+		check_input : bool, optional
+			Check to make sure that the observed symbol is a valid symbol for that
+			distribution to produce.
+
+		Returns
+		-------
+		probabilitie : array-like, shape (n_nodes)
+			An array of univariate distribution objects showing the probabilities
+			of each variable.
+		"""
 
 		return self.forward_backward( data, max_iterations, check_input )
 
 	def forward_backward( self, data={}, max_iterations=100, check_input=True ):
-		"""
-		Run loopy belief propogation on the underlying factor graph until
-		convergence, and return the marginals.
+		"""Returns the probabilities of each variable in the graph given evidence.
+
+		This calculates the marginal probability distributions for each state given
+		the evidence provided through loopy belief propogation. Loopy belief
+		propogation is an approximate algorithm which is exact for certain graph
+		structures.
+
+		Parameters
+		----------
+		data : dict or array-like, shape <= n_nodes, optional
+			The evidence supplied to the graph. This can either be a dictionary
+			with keys being state names and values being the observed values
+			(either the emissions or a distribution over the emissions) or an
+			array with the values being ordered according to the nodes incorporation
+			in the graph (the order fed into .add_states/add_nodes) and None for
+			variables which are unknown. If nothing is fed in then calculate the
+			marginal of the graph.
+		max_iterations : int, optional
+			The number of iterations with which to do loopy belief propogation.
+			Usually requires only 1.
+		check_input : bool, optional
+			Check to make sure that the observed symbol is a valid symbol for that
+			distribution to produce.
+
+		Returns
+		-------
+		probabilitie : array-like, shape (n_nodes)
+			An array of univariate distribution objects showing the probabilities
+			of each variable.
 		"""
 
 		if check_input:
@@ -135,20 +263,31 @@ cdef class BayesianNetwork( Model ):
 
 		return self.graph.forward_backward( data, max_iterations )
 
-	def from_sample( self, items, weights=None, inertia=0.0 ):
-		"""Another name for the train method."""
-
-		self.train( items, weights, inertia )
-
 	def fit( self, items, weights=None, inertia=0.0 ):
-		"""sklearn wrapper for the train method."""
+		"""Fit the model to data using MLE estimates.
 
-		self.train( items, weights, inertia )
+		Fit the model to the data by updating each of the components of the model,
+		which are univariate or multivariate distributions. This uses a simple
+		MLE estimate to update the distributions according to their summarize or
+		fit methods.
 
-	def train( self, items, weights=None, inertia=0.0 ):
-		"""
-		Take in data, with each column corresponding to observations of the
-		state, as ordered by self.states.
+		sklearn wrapper for the train method.
+
+		Parameters
+		----------
+		items : array-like, shape (n_samples, n_nodes)
+			The data to train on, where each row is a sample and each column
+			corresponds to the associated variable.
+		weights : array-like, shape (n_nodes), optional
+			The weight of each sample as a positive double
+		inertia : double, optional
+			The inertia for updating the distributions, passed along to the
+			distribution method.
+
+		Returns
+		-------
+		self : object
+			The fit Bayesian network object.
 		"""
 
 		indices = { state.distribution: i for i, state in enumerate( self.states ) }
@@ -164,11 +303,29 @@ cdef class BayesianNetwork( Model ):
 				state.distribution.from_sample( [ item[i] for item in items ], weights, inertia )
 
 		self.bake()
+		return self
 
 	def impute( self, items, max_iterations=100 ):
-		"""
-		Take in a matrix of data and impute all nan values to their MLE
-		values. 
+		"""Impute missing values of a data matrix using MLE.
+
+		Impute the missing values of a data matrix using the maximally likely
+		predictions according to the forward-backward algorithm. Run each
+		sample through the algorithm (predict_proba) and replace missing values
+		with the maximally likely predicted emission. 
+		
+		Parameters
+		----------
+		items : array-like, shape (n_samples, n_nodes)
+			Data matrix to impute. Missing values must be either None (if lists)
+			or np.nan (if numpy.ndarray). Will fill in these values with the
+			maximally likely ones.
+		max_iterations : int, optional
+			Number of iterations to run loopy belief propogation for.
+
+		Returns
+		-------
+		items : array-like, shape (n_samples, n_nodes)
+			This is the data matrix with the missing values imputed.
 		"""
 
 		for i in range( len(items) ):
