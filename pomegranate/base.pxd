1261d8e45f789c8b6fb076a4ca2fcd95b859c6f6
Nicolas Granger <nicolas.granger@telecom-sudparis.eu>
2016-09-05 18:08:59 +0200

    beautified pomegranate/base.{pxd,pyd} in compliance with sklearn style, compacted one liners

--- pomegranate/base.pxd
+++ pomegranate/base.pxd
@@ -1,13 +1,12 @@
 # base.pxd
 # Contact: Jacob Schreiber ( jmschreiber91@gmail.com )
 
-
 cimport numpy
 
-ctypedef numpy.npy_float64 DOUBLE_t 
-ctypedef numpy.npy_intp SIZE_t  
+ctypedef numpy.npy_intp SIZE_t
 
-cdef class Model( object ):
+
+cdef class Model(object):
 	cdef public str name
 	cdef public int d
 	cdef public bint frozen
@@ -16,15 +15,19 @@ cdef class Model( object ):
 	cdef double _log_probability( self, double symbol ) nogil
 	cdef double _mv_log_probability( self, double* symbol ) nogil
 	cdef double _vl_log_probability( self, double* symbol, int n ) nogil
-	cdef void _v_log_probability( self, double* symbol, double* log_probability, int n ) nogil
-	cdef double _summarize( self, double* items, double* weights, SIZE_t n ) nogil
+	cdef void _v_log_probability( self, double* symbol,
+	                              double* log_probability, int n ) nogil
+	cdef double _summarize( self, double* items, double* weights,
+	                        SIZE_t n ) nogil
 
-cdef class GraphModel( Model ):
+
+cdef class GraphModel(Model):
 	cdef public list states, edges
 	cdef public object graph
 	cdef int n_edges, n_states
 
-cdef class State( object ):
+
+cdef class State(object):
 	cdef public Model distribution
 	cdef public str name
 	cdef public double weight
--- pomegranate/base.pyx
+++ pomegranate/base.pyx
@@ -4,35 +4,29 @@
 from .utils cimport *
 
 from .distributions import Distribution
-from .gmm import GeneralMixtureModel
 
-import itertools as it
 import json
 import numpy
-import networkx
-import sys
 import uuid
 
-if sys.version_info[0] > 2:
-	# Set up for Python 3
-	xrange = range
 
 # Define some useful constants
 DEF NEGINF = float("-inf")
 DEF INF = float("inf")
 
-cdef class Model( object ):
+
+cdef class Model(object):
 	"""The abstract building block for all distributions."""
 
-	def __cinit__( self ):
+	def __cinit__(self):
 		self.name = "Model"
 		self.frozen = False
 		self.d = 0
 
-	def __str__( self ):
+	def __str__(self):
 		return self.to_json()
 
-	def __repr__( self ):
+	def __repr__(self):
 		return self.to_json()
 
 	def get_params(self, *args, **kwargs):
@@ -41,14 +35,23 @@ cdef class Model( object ):
 	def set_params(self, state):
 		self.__setstate__(state)
 
-	def copy( self ):
+	def to_json(self):
+		"""Serialize this object into JSON format."""
+		raise NotImplementedError
+
+	@classmethod
+	def from_json( cls, json ):
+		"""Deserialize this object from its JSON representation."""
+		raise NotImplementedError
+
+	def copy(self):
 		"""Return a deep copy of this distribution object.
 
 		This object will not be tied to any other distribution or connected
 		in any form.
 
-		Paramters
-		---------
+		Parameters
+		----------
 		None
 
 		Returns
@@ -56,39 +59,19 @@ cdef class Model( object ):
 		distribution : Distribution
 			A copy of the distribution with the same parameters.
 		"""
-
 		return self.__class__.from_json( self.to_json() )
 
-	def freeze( self ):
-		"""Freeze the distribution, preventing updates from occuring.
-
-		Parameters
-		----------
-		None
-
-		Returns
-		-------
-		None
-		"""
-
+	def freeze(self):
+		"""Freeze the distribution, preventing updates from occuring."""
 		self.frozen = True
 
-	def thaw( self ):
-		"""Thaw the distribution, re-allowing updates to occur.
-
-		Parameters
-		----------
-		None
-
-		Returns
-		-------
-		None
-		"""
-
+	def thaw(self):
+		"""Thaw the distribution, re-allowing updates to occur."""
 		self.frozen = False
 
 	def log_probability( self, double symbol ):
-		"""Return the log probability of the given symbol under this distribution.
+		"""Return the log probability of the given symbol under this
+		distribution.
 
 		Parameters
 		----------
@@ -101,17 +84,16 @@ cdef class Model( object ):
 		logp : double
 			The log probability of that point under the distribution.
 		"""
-
 		return NotImplementedError
 
-	def copy( self ):
+	def copy(self):
 		"""Return a deep copy of this distribution object.
 
 		This object will not be tied to any other distribution or connected
 		in any form.
 
-		Paramters
-		---------
+		Parameters
+		----------
 		None
 
 		Returns
@@ -119,8 +101,7 @@ cdef class Model( object ):
 		distribution : Distribution
 			A copy of the distribution with the same parameters.
 		"""
-
-		return self.__class__( *self.parameters )
+		return self.__class__(*self.parameters)
 
 	def sample( self, n=None ):
 		"""Return a random item sampled from this distribution.
@@ -128,8 +109,8 @@ cdef class Model( object ):
 		Parameters
 		----------
 		n : int or None, optional
-			The number of samples to return. Default is None, which is to generate
-			a single sample.
+			The number of samples to return. Default is None, which is to
+			generate a single sample.
 
 		Returns
 		-------
@@ -137,7 +118,6 @@ cdef class Model( object ):
 			Returns a sample from the distribution of a type in the support
 			of the distribution.
 		"""
-
 		raise NotImplementedError
 
 	def probability( self, symbol ):
@@ -153,11 +133,11 @@ cdef class Model( object ):
 		probability : double
 			The probability of that point under the distribution.
 		"""
-
 		return numpy.exp(self.log_probability(symbol))
 
 	def log_probability( self, symbol ):
-		"""Return the log probability of the given symbol under this distribution.
+		"""Return the log probability of the given symbol under this
+		distribution.
 
 		Parameters
 		----------
@@ -170,7 +150,6 @@ cdef class Model( object ):
 		logp : double
 			The log probability of that point under the distribution.
 		"""
-
 		raise NotImplementedError
 
 	def sample( self, n=None ):
@@ -179,8 +158,8 @@ cdef class Model( object ):
 		Parameters
 		----------
 		n : int or None, optional
-			The number of samples to return. Default is None, which is to generate
-			a single sample.
+			The number of samples to return. Default is None, which is to
+			generate a single sample.
 
 		Returns
 		-------
@@ -188,7 +167,6 @@ cdef class Model( object ):
 			Returns a sample from the distribution of a type in the support
 			of the distribution.
 		"""
-
 		raise NotImplementedError
 
 	def fit( self, items, weights=None, inertia=0.0 ):
@@ -208,20 +186,20 @@ cdef class Model( object ):
 
 		inertia : double, optional
 			The weight of the previous parameters of the model. The new
-			parameters will roughly be old_param*inertia + new_param*(1-inertia),
-			so an inertia of 0 means ignore the old parameters, whereas an
-			inertia of 1 means ignore the new parameters. Default is 0.0.
+			parameters will roughly be
+			old_param * inertia + new_param * (1-inertia), so an inertia of 0
+			means ignore the old parameters, whereas an inertia of 1 means
+			ignore the new parameters. Default is 0.0.
 
 		Returns
 		-------
 		None
 		"""
-
 		raise NotImplementedError
 
 	def summarize( self, items, weights=None ):
-		"""Summarize a batch of data into sufficient statistics for a later update.
-
+		"""Summarize a batch of data into sufficient statistics for a later
+		update.
 
 		Parameters
 		----------
@@ -239,7 +217,6 @@ cdef class Model( object ):
 		-------
 		None
 		"""
-
 		return NotImplementedError
 
 	def from_summaries( self, inertia=0.0 ):
@@ -249,29 +226,19 @@ cdef class Model( object ):
 		----------
 		inertia : double, optional
 			The weight of the previous parameters of the model. The new
-			parameters will roughly be old_param*inertia + new_param*(1-inertia),
-			so an inertia of 0 means ignore the old parameters, whereas an
-			inertia of 1 means ignore the new parameters. Default is 0.0.
+			parameters will roughly be
+			old_param * inertia + new_param * (1-inertia), so an inertia of 0
+			means ignore the old parameters, whereas an inertia of 1 means
+			ignore the new parameters. Default is 0.0.
 
 		Returns
 		-------
 		None
 		"""
-
 		return NotImplementedError
 
-	def clear_summaries( self ):
-		"""Clear the summary statistics stored in the object.
-
-		Parameters
-		----------
-		None
-
-		Returns
-		-------
-		None
-		"""
-
+	def clear_summaries(self):
+		"""Clear the summary statistics stored in the object."""
 		return NotImplementedError
 
 	cdef double _log_probability( self, double symbol ) nogil:
@@ -283,14 +250,16 @@ cdef class Model( object ):
 	cdef double _vl_log_probability( self, double* symbol, int n ) nogil:
 		return NEGINF
 
-	cdef double _summarize( self, double* items, double* weights, int n ) nogil:
+	cdef double _summarize( self, double* items,
+	                        double* weights, int n ) nogil:
 		pass
 
-	cdef void _v_log_probability( self, double* symbol, double* log_probability, int n ) nogil:
+	cdef void _v_log_probability( self, double* symbol,
+								  double* log_probability, int n ) nogil:
 		pass
 
 
-cdef class GraphModel( Model ):
+cdef class GraphModel(Model):
 	"""Represents an generic graphical model."""
 
 	def __init__( self, name=None ):
@@ -308,40 +277,25 @@ cdef class GraphModel( Model ):
 		self.d = 0
 
 	def __str__(self):
-		"""
-		Represent this model with it's name and states.
-		"""
-
+		"""Represent this model with it's name and states."""
 		return "{}:{}".format(self.name, "".join(map(str, self.states)))
 
 	def add_node( self, node ):
-		"""
-		Add a node to the graph.
-		"""
-
-		self.states.append( node )
+		"""Add a node to the graph."""
+		self.states.append(node)
 		self.n_states += 1
 
 	def add_nodes( self, *nodes ):
-		"""
-		Add multiple states to the graph.
-		"""
-
+		"""Add multiple states to the graph."""
 		for node in nodes:
-			self.add_node( node )
+			self.add_node(node)
 
 	def add_state( self, state ):
-		"""
-		Another name for a node.
-		"""
-
-		self.add_node( state )
+		"""Another name for a node."""
+		self.add_node(state)
 
 	def add_states( self, *states ):
-		"""
-		Another name for a node.
-		"""
-
+		"""Another name for a node."""
 		for state in states:
 			self.add_state(state)
 
@@ -356,34 +310,22 @@ cdef class GraphModel( Model ):
 		self.n_edges += 1
 
 	def add_transition( self, a, b ):
-		"""
-		Transitions and edges are the same.
-		"""
-
+		"""Transitions and edges are the same."""
 		self.add_edge( a, b )
 
 	def node_count( self):
-		"""
-		Returns the number of nodes/states in the model
-		"""
-
+		"""Returns the number of nodes/states in the model"""
 		return self.n_states
 
-	def state_count( self ):
-		"""
-		Returns the number of states present in the model.
-		"""
-
+	def state_count(self):
+		"""Returns the number of states present in the model."""
 		return self.n_states
 
-	def edge_count( self ):
-		"""
-		Returns the number of edges present in the model.
-		"""
-
+	def edge_count(self):
+		"""Returns the number of edges present in the model."""
 		return self.n_edges
 
-	def dense_transition_matrix( self ):
+	def dense_transition_matrix(self):
 		"""
 		Returns the dense transition matrix. Useful if the transitions of
 		somewhat small models need to be analyzed.
@@ -392,15 +334,15 @@ cdef class GraphModel( Model ):
 		m = len(self.states)
 		transition_log_probabilities = numpy.zeros( (m, m) ) + NEGINF
 
-		for i in xrange(m):
-			for n in xrange( self.out_edge_count[i], self.out_edge_count[i+1] ):
+		for i in range(m):
+			for n in range( self.out_edge_count[i], self.out_edge_count[i+1] ):
 				transition_log_probabilities[i, self.out_transitions[n]] = \
 					self.out_transition_log_probabilities[n]
 
 		return transition_log_probabilities
 
 
-cdef class State( object ):
+cdef class State(object):
 	"""
 	Represents a state in an HMM. Holds emission distribution, but not
 	transition distribution, because that's stored in the graph edges.
@@ -423,21 +365,19 @@ cdef class State( object ):
 		# Save the weight, or default to the unit weight
 		self.weight = weight or 1.
 
-	def __reduce__( self ):
+	def __reduce__(self):
 		return self.__class__, (self.distribution, self.name, self.weight)
 
-	def __str__( self ):
+	def __str__(self):
 		"""
 		The string representation of a state is the json, so call that format.
 		"""
-
 		return self.to_json()
 
-	def __repr__( self ):
+	def __repr__(self):
 		"""
 		The string representation of a state is the json, so call that format.
 		"""
-
 		return self.__str__()
 
 	def tie( self, state ):
@@ -445,56 +385,48 @@ cdef class State( object ):
 		Tie this state to another state by just setting the distribution of the
 		other state to point to this states distribution.
 		"""
-
 		state.distribution = self.distribution
 
-	def is_silent( self ):
+	def is_silent(self):
 		"""
 		Return True if this state is silent (distribution is None) and False
 		otherwise.
 		"""
-
 		return self.distribution is None
 
-	def tied_copy( self ):
+	def tied_copy(self):
 		"""
 		Return a copy of this state where the distribution is tied to the
 		distribution of this state.
 		"""
-
 		return State( distribution=self.distribution, name=self.name+'-tied' )
 
 	def copy( self ):
-		"""
-		Return a hard copy of this state.
-		"""
-
+		"""Return a hard copy of this state."""
 		return State( distribution=self.distribution.copy(), name=self.name )
 
 	def to_json( self, separators=(',', ' : '), indent=4 ):
-		"""
-		Convert this state to JSON format.
-		"""
+		"""Convert this state to JSON format."""
 
 		return json.dumps( {
-							    'class' : 'State',
-								'distribution' : None if self.is_silent() else json.loads( self.distribution.to_json() ),
-								'name' : self.name,
-								'weight' : self.weight
-							}, separators=separators, indent=indent )
+				'class' : 'State',
+				'distribution' : None if self.is_silent()
+					else json.loads( self.distribution.to_json() ),
+				'name' : self.name,
+				'weight' : self.weight
+			}, separators=separators, indent=indent )
 
 	@classmethod
 	def from_json( cls, s ):
-		"""
-		Read a State from a given string formatted in JSON.
-		"""
+		"""Read a State from a given string formatted in JSON."""
 
 		# Load a dictionary from a JSON formatted string
-		d = json.loads( s )
+		d = json.loads(s)
 
 		# If we're not decoding a state, we're decoding the wrong thing
 		if d['class'] != 'State':
-			raise IOError( "State object attempting to decode {} object".format( d['class'] ) )
+			raise IOError( "State object attempting to decode "
+			               "{} object".format( d['class'] ) )
 
 		# If this is a silent state, don't decode the distribution
 		if d['distribution'] is None:
@@ -508,4 +440,6 @@ cdef class State( object ):
 		dist = eval(c).from_json( json.dumps( d['distribution'] ) )
 		return cls( dist, name, weight )
 
+
+# Create a convenient alias
 Node = State
