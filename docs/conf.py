b59d20f0bdd87d005794daed6361ebdeab2748c1
Jacob Schreiber <jmschreiber91@gmail.com>
2016-11-08 12:19:11 -0800

    a

--- docs/conf.py
+++ docs/conf.py
@@ -15,13 +15,18 @@
 import sys
 import os
 import subprocess
+import mock
+
+MOCK_MODULES = ['numpy', 'scipy', 'joblib', 'networkx', 'cython']
+for mod_name in MOCK_MODULES:
+   sys.modules[mod_name] = mock.Mock() 
 
 subprocess.call('pip install numpydoc', shell=True)
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 # -- General configuration ------------------------------------------------
 
