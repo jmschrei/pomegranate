b1b3d22706797ff6c77219381df3e57ec8a55b26
Eric M Whitmire <emwhit@cs.washington.edu>
2017-04-27 19:56:39 -0700

    Building python wheel Added AppVeyor testing and wheel generation Added Travis CI testing and wheel for OS X Numpy no longer required prior to installation

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
-from distutils.core import setup
-from distutils.extension import Extension
-import numpy as np
+from setuptools import setup
+from setuptools import Extension
+from setuptools.command.build_ext import build_ext as _build_ext
 
 try:
     from Cython.Build import cythonize
@@ -24,18 +24,24 @@ filenames = [ "base",
 
 if not use_cython:
     extensions = [
-        Extension( "pomegranate.{}".format( name ), 
-                   [ "pomegranate/{}.{}".format( name, ext ) ], 
-                   include_dirs=[np.get_include()] ) for name in filenames
+        Extension( "pomegranate.{}".format( name ),
+                   [ "pomegranate/{}.{}".format( name, ext ) ]) for name in filenames
     ]
 else:
     extensions = [
             Extension( "pomegranate.*", 
-                       [ "pomegranate/*.pyx" ], 
-                       include_dirs=[np.get_include()] )
+                       [ "pomegranate/*.pyx" ] )
     ]
 
     extensions = cythonize( extensions )
+
+class build_ext(_build_ext):
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        # Prevent numpy from thinking it is still in its setup process:
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(numpy.get_include())
 
 setup(
     name='pomegranate',
@@ -47,11 +53,17 @@ setup(
     license='LICENSE.txt',
     description='Pomegranate is a graphical models library for Python, implemented in Cython for speed.',
     ext_modules=extensions,
-    install_requires=[
+    cmdclass={'build_ext':build_ext},
+    setup_requires=[
         "cython >= 0.22.1",
+        "numpy >= 1.8.0",
+        "scipy >= 0.17.0"
+    ],
+    install_requires=[
         "numpy >= 1.8.0",
         "joblib >= 0.9.0b4",
         "networkx >= 1.8.1",
         "scipy >= 0.17.0"
     ],
+    test_suite = 'nose.collector'
 )
