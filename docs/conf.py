5510051438d87c5dd5f1193f5ef61c5ec38978f2
Jacob Schreiber <jmschreiber91@gmail.com>
2018-06-16 10:58:23 -0700

    FIX mock docs

--- docs/conf.py
+++ docs/conf.py
@@ -28,6 +28,12 @@ subprocess.call('pip install numpydoc', shell=True)
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('../..'))
 
+import mock
+
+MOCK_MODULES = ['numpy', 'scipy', 'joblib', 'networkx', 'cython', 'wheel', 'nose', 'scipy.interpolate', 'scipy.special']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
