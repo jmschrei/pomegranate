c1f96111607f8e8a03d26224d856dd438f4acc91
Alex Henrie <alexhenrie24@gmail.com>
2020-05-11 14:31:55 -0600

    Get rid of a couple more calls to calloc and numpy.zeros

--- pomegranate/MarkovNetwork.pyx
+++ pomegranate/MarkovNetwork.pyx
@@ -488,7 +488,7 @@ cdef class MarkovNetwork(Model):
 			raise ValueError("must bake model before summarizing data")
 
 		n, d = len(X), len(X[0])
-		X_int = numpy.zeros((n, d), dtype='float64')
+		X_int = numpy.empty((n, d), dtype='float64')
 
 		for i in range(n):
 			for j in range(d):
