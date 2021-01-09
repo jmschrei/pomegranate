627cb05225147f231d80a9df7a5b9b9d6acfb66a
Alex Henrie <alexhenrie24@gmail.com>
2021-01-09 12:53:23 -0700

    Tell Pip to install Cython, NumPy, and SciPy before running setup.py (#860)
    [845] Installation via pip while missing cython fails

--- setup.py
+++ setup.py
@@ -64,7 +64,8 @@ class build_ext(_build_ext):
     def finalize_options(self):
         _build_ext.finalize_options(self)
         # Prevent numpy from thinking it is still in its setup process:
-        __builtins__.__NUMPY_SETUP__ = False
+        if hasattr(__builtins__, '__NUMPY_SETUP__'):
+            __builtins__.__NUMPY_SETUP__ = False
         import numpy
         self.include_dirs.append(numpy.get_include())
 
