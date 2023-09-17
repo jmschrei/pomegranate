5f924c8ee70007cecba53197160df558fb82d231
Jacob Schreiber <jmschreiber91@gmail.com>
2023-09-17 11:49:13 -0700

    v0.14.9

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ class build_ext(_build_ext):
 
 setup(
     name='pomegranate',
-    version='0.14.8',
+    version='0.14.9',
     author='Jacob Schreiber',
     author_email='jmschreiber91@gmail.com',
     packages=[
